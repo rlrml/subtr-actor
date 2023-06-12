09300f347b25fe2dbb89f388b58cb2bc0834ea36
Ivan Malison <IvanMalison@gmail.com>
2023-06-12 13:26:38 -0600

    Rewrite some portions of top level doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,26 @@
 //! # subtr-actor
 //!
-//! [`subtr-actor`](crate) is a versatile library designed to facilitate the process of
-//! working with and extracting data from Rocket League replays. Utilizing the
-//! powerful [`boxcars`] library for parsing, subtr-actor simplifies the
-//! underlying complex actor-based structure of replay files, making them more
-//! accessible and easier to manipulate.
+//! [`subtr-actor`](crate) is a versatile library designed to facilitate the
+//! process of working with and extracting data from Rocket League replays.
+//! Utilizing the powerful [`boxcars`] library for parsing, subtr-actor
+//! simplifies the underlying actor-based structure of replay files, making them
+//! more accessible and easier to manipulate.
 //!
 //! ## Overview of Key Components
 //!
 //! - **[`ReplayProcessor`]**: This struct is at the heart of subtr-actor's
-//! replay processing capabilities. The [`ReplayProcessor`] traverses the actor
-//! graph, pushing frames through an [`ActorStateModeler`] to capture the state of
-//! all actors at any given moment. It provides a suite of helper methods to
-//! assist in the navigation of the actor graph and the retrieval of
-//! information about the game as it progresses.
+//! replay processing capabilities. In its main entry point,
+//! [`ReplayProcessor::process`], it pushes network frames from the
+//! [`boxcars::Replay`] that it is initialized with though an
+//! [`ActorStateModeler`] instance, calling the [`Collector`] instance that is
+//! provided as an argument as it does so. The [`Collector`] is provided with a
+//! reference to the [`ReplayProcessor`] each time the it is invoked, which
+//! allows it to use the suite of helper methods which greatly assist in the
+//! navigation of the actor graph and the retrieval of information about the
+//! current game state.
 //!
 //! - **[`Collector`]**: This trait outlines the blueprint for data
-//! collection from replays. The Collector interfaces with a ReplayProcessor,
+//! collection from replays. The Collector interfaces with a [`ReplayProcessor`],
 //! handling frame data and guiding the pace of replay progression. It is
 //! typically invoked repeatedly through the [`ReplayProcessor::process`] method
 //! as the replay is processed frame by frame.
@@ -31,19 +35,19 @@
 //! This representation is ideal for use with machine learning libraries like
 //! pytorch and tensorflow.
 //!
-//! - **[`ReplayData`]**: This [`Collector`] implementation provides an easy way to
-//! get a serializable to e.g. json (though [`serde::Serialize`]) representation
-//! of the replay. The representation differs from what you might get from e.g.
-//! raw boxcars in that it is not a complicated graph of actor objects and is
-//! instead something more akin to the way a human might think of the data
-//! contained in a replay.
+//! - **[`ReplayData`]**: This [`Collector`] implementation provides an easy way
+//! to get a serializable to e.g. json (though [`serde::Serialize`])
+//! representation of the replay. The representation differs from what you might
+//! get from e.g. raw boxcars in that it is not a complicated graph of actor
+//! objects, but instead something more natural where the data associated with
+//! each entity in the game is grouped together.
 
-mod actor_state;
-mod collector;
-mod constants;
-mod error;
-mod processor;
-mod util;
+pub mod actor_state;
+pub mod collector;
+pub mod constants;
+pub mod error;
+pub mod processor;
+pub mod util;
 
 #[cfg(test)]
 mod util_test;
