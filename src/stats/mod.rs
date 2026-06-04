9a11ab255bb8bebd8262ec32eda0cacb64e40a37
Ivan Malison <IvanMalison@gmail.com>
2026-06-03 20:01:44 -0700

    Remove unused flip reset tuning manifest

--- src/stats/mod.rs
+++ src/stats/mod.rs
@@ -4,10 +4,8 @@ pub(crate) mod calculators;
 pub(crate) mod common;
 pub mod export;
 pub mod timeline;
-pub mod tuning;
 
 pub use accumulators::*;
 pub use calculators::*;
 pub use export::*;
 pub use timeline::*;
-pub use tuning::*;
--- src/stats/tuning/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod flip_reset;
-
-pub use flip_reset::*;
